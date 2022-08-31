dc80f211a96f9abb5aefe05c3fa214591bf18a7d
Philip Oka <philip.oka@gmail.com>
2022-08-31 16:35:29 -0300

    - Simplifying the db_schema_builder to return the schema and not export;

--- db_schema_builder.rb
+++ db_schema_builder.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'json'
 require './tables_name_source'
 require './bubble_table_fetcher'
 require './bubble_api_service'
+require './exporter_service'
 
 # class DbSchemaBuilder
 class DbSchemaBuilder
@@ -17,8 +17,7 @@ class DbSchemaBuilder
 
   def call
     @table_names.each { |name| build_schema(name) }
-    export_schema
-    self
+    @schema
   end
 
   private
@@ -26,32 +25,15 @@ class DbSchemaBuilder
   def build_schema(name)
     table = {}
     table[:name] = name
-    table[:body] = @fetcher.call(name.downcase.gsub(' ', ''))
+    table[:body] = @fetcher.call(name)
     @schema << table
-  end
-
-  def export_schema
-    if Dir.children('./').include?('schema.rb')
-      export_sequence_schema
-    else
-      export_first_schema
-    end
-  end
-
-  def export_sequence_schema
-    last_schema = Dir.children('./').select { |name| name.match?(/schema\d*.rb/) }.last
-    new_export_index = last_schema[/schema(.*?).rb/].to_i + 1
-    File.write("schema#{new_export_index}.rb", @schema)
-    File.write("schema#{new_export_index}.json", @schema.to_json)
-  end
-
-  def export_first_schema
-    File.write('schema.json', @schema.to_json)
-    File.write('schema.rb', @schema)
   end
 end
 
-bubble_api_service = BubbleApiService.new
-bubble_table_fetcher = BubbleTableFetcher.new(bubble_api_service)
+# bubble_api_service = BubbleApiService.new
+# bubble_table_fetcher = BubbleTableFetcher.new(bubble_api_service)
 
-DbSchemaBuilder.new(bubble_table_fetcher, TABLE_NAMES).call
+# schema_builder = DbSchemaBuilder.new(bubble_table_fetcher, TABLE_NAMES)
+# schema = schema_builder.call
+
+# ExporterService.write('schema', schema)
--- spec/db_schema_builder_spec.rb
+++ spec/db_schema_builder_spec.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 require_relative './../db_schema_builder'
-require_relative './../tables_name_source'
 
 RSpec.describe DbSchemaBuilder do
-  let(:db_schema_builder) { described_class.new(double, TABLE_NAMES) }
+  let(:db_schema_builder) { described_class.new(double, ['Nurse']) }
   describe '.new' do
     context '#schema' do
       it 'returns an empty array' do
@@ -15,10 +14,6 @@ RSpec.describe DbSchemaBuilder do
     describe '#call' do
       it 'returns an instance of itself' do
         expect(db_schema_builder.call).to be_a(described_class)
-      end
-
-      it 'generates a schema file' do
-        expect(File.exist?('schema.rb')).to be_truthy
       end
     end
   end
