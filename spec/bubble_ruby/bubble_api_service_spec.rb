f53b053dabfd3f5abaa48097ffa1660c6084320a
Philip Oka <philip.oka@gmail.com>
2023-05-12 15:40:54 -0300

    Commenting out old specs

--- spec/bubble_ruby/bubble_api_service_spec.rb
+++ spec/bubble_ruby/bubble_api_service_spec.rb
@@ -1,27 +1,27 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'bubble_ruby/bubble_api_service'
+# require 'bubble_ruby/bubble_api_service'
 
-module BubbleRuby
-  RSpec.describe BubbleApiService do
-    describe '#fetch_records' do
-      context 'with two arguments ("Shift", 5)' do
-        it 'returns an array of data with length of 5' do
-          expect(subject.fetch_records('Shift', 5)['results'].length).to eq(5)
-        end
-      end
+# module BubbleRuby
+#   RSpec.describe BubbleApiService do
+#     describe '#fetch_records' do
+#       context 'with two arguments ("Shift", 5)' do
+#         it 'returns an array of data with length of 5' do
+#           expect(subject.fetch_records('Shift', 5)['results'].length).to eq(5)
+#         end
+#       end
 
-      context 'with only one argument/name ("Shift")' do
-        it 'returns an array of a single data point' do
-          expect(subject.fetch_records('Shift')['results'].length).to eq(1)
-        end
-      end
+#       context 'with only one argument/name ("Shift")' do
+#         it 'returns an array of a single data point' do
+#           expect(subject.fetch_records('Shift')['results'].length).to eq(1)
+#         end
+#       end
 
-      context 'without any arguments' do
-        it 'should raise an error' do
-          expect { subject.fetch_records }.to raise_error(ArgumentError)
-        end
-      end
-    end
-  end
-end
+#       context 'without any arguments' do
+#         it 'should raise an error' do
+#           expect { subject.fetch_records }.to raise_error(ArgumentError)
+#         end
+#       end
+#     end
+#   end
+# end
