f98dd46280ce0fce30600db19b6a323dc8e473df
Philip Oka <philip.oka@gmail.com>
2023-05-12 15:44:43 -0300

    refactoring Tables factory to be more cohesive and creating the Response model

--- lib/bubble_ruby/db/schema/tables/build.rb
+++ lib/bubble_ruby/db/schema/tables/build.rb
@@ -3,41 +3,22 @@
 module BubbleRuby
   require_relative '../table'
   require_relative '../table/column'
-  module DB::Schema::Tables
-    require_relative 'fetch'
+  class DB::Schema::Tables
     module Build
-      extend self
-
-      def call(endpoint:)
-        response    = Fetch.call(endpoint: endpoint)
-        table_names = extract_table_names(response)
-        tables      = table_names.map { |name| DB::Schema::Table.new(name: name) }
-        write_table_body(response, table_names, tables)
-        tables
-      end
-
-      private
-
-      def extract_table_names(response)
-        objs = response['paths'].select { |k, _v| k.match?(%r{/obj/(.*)/{UniqueID}}) }
-        objs.map { |k, _v| k.match(%r{/obj/(.*)/{UniqueID}}).captures }.flatten
-      end
-
-      def write_table_body(response, table_names, tables)
-        bodies = response['definitions'].select { |k, _v| table_names.include?(k) }
-        bodies.each do |key, value|
-          table = find_table(tables, name: key)
-          next unless table
-
-          value['properties'].each do |key_prop, value_prop|
-            column = BubbleRuby::DB::Schema::Table::Column.new(table: table.name, name: key_prop, type: value_prop)
+      def self.call(endpoint:)
+        response = Response.new(endpoint: endpoint)
+        response.tables_response.map do |table_name, table_body|
+          table = DB::Schema::Table.new(name: table_name)
+          table_body['properties'].each do |column_name, column_type|
+            column = DB::Schema::Table::Column.new(
+              table_name: table.name,
+              name: column_name,
+              type: column_type
+            )
             table.body << column
           end
+          table
         end
-      end
-
-      def find_table(tables, name:)
-        tables.find { |table| table.name == name }
       end
     end
   end
